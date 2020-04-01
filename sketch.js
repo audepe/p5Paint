3d64c775ff60653441f3c8699ff7a72c6ab7e67a
audepe <danieldelgadoperera@gmail.com>
2020-04-01 13:58:30 +0100

    Añadido selector de color y previsualización

--- sketch.js
+++ sketch.js
@@ -5,9 +5,11 @@ function setup() {
 
   slider = createSlider(1,50,25, 1);
   button = createButton('Borrar');
+  colorPicker = createColorPicker('#ed225d');
 
-  slider.position(15,15);
-  button.position(150,15);
+  slider.position(60,15);
+  button.position(200,20);
+  colorPicker.position(265, 15);
 
   button.mousePressed(() => background(255));
 }
@@ -17,12 +19,27 @@ function draw() {
   noStroke();
   fill(200);
   rect(0, 0, windowWidth, windowHeight/16);
+  fill(colorPicker.color())
+  circle(30,30,slider.value());
   pop();
+}
+
+function mousePressed(){
+  if( mouseY > windowHeight/16){
+    push();
+    fill(colorPicker.color());
+    stroke(colorPicker.color());
+    circle(mouseX,mouseY,slider.value());
+    pop();
+  }    
 }
 
 function mouseDragged(){
   if( mouseY > windowHeight/16){
+    push();
+    stroke(colorPicker.color());
     strokeWeight(slider.value());
     line(mouseX, mouseY, pmouseX, pmouseY);
+    pop();
   }  
 }
